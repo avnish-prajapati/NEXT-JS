302a9c1c2c76da985389c77fe008be195f26a746
Avnish Prajapati <avnishprajapati664@gmail.com>
2026-08-22 22:44:18 +0530

    Update page.jsx

--- test-module/src/app/Loginpage/page.jsx
+++ test-module/src/app/Loginpage/page.jsx
@@ -3,7 +3,8 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function LoginPage() {
+export default function LoginPage() 
+{
   const router = useRouter();
 
   const [email, setEmail] = useState("");
